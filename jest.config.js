a79424dbef38f0e00b7e2ac949ca9d283917429d
Gideon Adeti <gideonadeti0@gmail.com>
2024-07-08 13:11:15 +0000

    Configure jest and babel; add a boilerplate test to ensure configuration works

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  transform: {
+    "^.+\\.js$": "babel-jest",
+  },
+  moduleFileExtensions: ["js"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
+};
