2ca3989cef5a3654506f5764e24093838afba0a3
Gideon Adeti <gideonadeti0@gmail.com>
2024-07-08 13:24:05 +0000

    Implement ship class and its tests

---
+++ __tests__/ship.test.js
@@ -0,0 +1,27 @@
+import Ship from "../src/models/ship";
+
+test("Ship should have correct length", () => {
+  const ship = new Ship(3);
+  expect(ship.length).toBe(3);
+});
+
+test("Ship should record hits", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+});
+
+test("Ship should sink when hits equal length", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("Ship should not sink when hits are less than length", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
