a76500c636080ebfd2a09aa72b30d6bcfb0aeaf6
Gideon Adeti <gideonadeti0@gmail.com>
2024-07-08 17:53:40 +0000

    Create boards after DOM is done loading

--- src/index.js
+++ src/index.js
@@ -8,3 +8,7 @@ import UI from './views/ui'
 import GameBoard from './models/game-board'
 import Player from './models/player'
 import Ship from './models/ship'
+
+document.addEventListener('DOMContentLoaded', () => {
+  UI.initialize()
+})
--- src/views/ui.js
+++ src/views/ui.js
@@ -1,3 +1,50 @@
 export default class UI {
+  static initialize () {
+    this.createBoards()
+  }
 
+  static createBoards () {
+    const boards = document.querySelectorAll('.game-board')
+
+    boards.forEach((board) => {
+      // Create column labels
+      const columnLabels = [
+        '',
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J'
+      ]
+      columnLabels.forEach((label) => {
+        const labelDiv = document.createElement('div')
+        labelDiv.textContent = label
+        labelDiv.classList.add('cell', 'label')
+        board.appendChild(labelDiv)
+      })
+
+      // Create rows
+      for (let row = 1; row <= 10; row++) {
+        // Create row label
+        const rowLabelDiv = document.createElement('div')
+        rowLabelDiv.textContent = row
+        rowLabelDiv.classList.add('cell', 'label')
+        board.appendChild(rowLabelDiv)
+
+        // Create cells for the row
+        for (let col = 1; col <= 10; col++) {
+          const cellDiv = document.createElement('div')
+          cellDiv.classList.add('cell')
+          cellDiv.dataset.row = row
+          cellDiv.dataset.col = col
+          board.appendChild(cellDiv)
+        }
+      }
+    })
+  }
 }
