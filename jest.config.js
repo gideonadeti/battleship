a153a750a6ba3cd3d95ee35d870b80ed7dcc3958
Gideon Adeti <gideonadeti0@gmail.com>
2025-11-22 19:46:50 +0000

    Add initial game assets and HTML structure for Battleship project, including fonts, favicons, sound effects, and main game interface in index.html.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest'
-  },
-  moduleFileExtensions: ['js'],
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
-  testPathIgnorePatterns: ['/node_modules/', '/__tests__/helpers/', '/__tests__/integration/'],
-  testEnvironment: 'jsdom'
-}
